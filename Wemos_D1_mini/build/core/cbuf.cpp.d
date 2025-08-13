428e494f379e1425c278b3716e943d6de8cba1a9
Max <zenchmax@gmail.com>
2025-08-13 11:12:13 +0300

    Ignore

--- Wemos_D1_mini/build/core/cbuf.cpp.d
+++
@@ -1,6 +0,0 @@
-D:\Max\WEMOS\Hi-Tech-Clock\Wemos_D1_mini\build\core\cbuf.cpp.o: \
- C:\Users\Max\AppData\Local\Arduino15\packages\esp8266\hardware\esp8266\3.1.2\cores\esp8266\cbuf.cpp \
- C:\Users\Max\AppData\Local\Arduino15\packages\esp8266\hardware\esp8266\3.1.2\cores\esp8266\CommonHFile.h \
- D:\Max\WEMOS\Hi-Tech-Clock\Wemos_D1_mini\build\core\Wemos_D1_mini.ino.globals.h \
- C:\Users\Max\AppData\Local\Arduino15\packages\esp8266\hardware\esp8266\3.1.2\cores\esp8266\cbuf.h \
- C:\Users\Max\AppData\Local\Arduino15\packages\esp8266\hardware\esp8266\3.1.2/tools/sdk/include/c_types.h
